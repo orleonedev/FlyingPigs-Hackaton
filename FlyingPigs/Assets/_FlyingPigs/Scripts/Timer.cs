8c5c6d840ed2e9ea242ba7a67d0664bea12403bd
UnbuiltThunder4 <unbuiltthunder4@gmail.com>
2023-11-04 12:10:44 +0100

    Changes to Timer and Audio Manager

--- FlyingPigs/Assets/_FlyingPigs/Scripts/Timer.cs
+++ FlyingPigs/Assets/_FlyingPigs/Scripts/Timer.cs
@@ -1,23 +1,33 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    [SerializeField] private TMP_Text score;
+    [SerializeField] private GameObject bomb;
+    [SerializeField] private GameObject boom;
+    [SerializeField] private Slider slider;
+    [SerializeField] private AudioManager audioManager;
     private float countdown = 10.0f; // Set the initial countdown time in seconds
     private bool isCounting = true;
+
+    private void Start() {
+        audioManager.PlaySound(audioManager.tickingClip);
+    }
 
     private void Update()
     {
         if (isCounting) {
             countdown -= Time.deltaTime; // Decrease countdown by time passed since the last frame
-            score.text = Mathf.Floor(countdown).ToString();
+            slider.value = countdown/10f;
 
             if (countdown <= 0.0f) {
                 // Countdown has reached 0, perform your desired action
-                Debug.Log("Countdown reached 0. Timer stopped.");
-                score.text = "0";
+                countdown = 0f;
                 isCounting = false; // Stop the timer
+                bomb.SetActive(false);
+                boom.SetActive(true);
+                audioManager.PlaySound(audioManager.timeUpClip);
             }
         }
     }
@@ -27,13 +37,15 @@ public class Timer : MonoBehaviour
         isCounting = false; // Call this function to stop the timer
     }
 
-    public void RestartTimer(float time = 5.0f)
+    public void RestartTimer(float time = 10.0f)
     {
         isCounting = true; // Call this function to restart the timer
         countdown = time;
+        bomb.SetActive(true);
+        boom.SetActive(false);
     }
 
-    public bool GetCounting(){
+    public bool GetCounting() {
         return isCounting;
     }
 }
