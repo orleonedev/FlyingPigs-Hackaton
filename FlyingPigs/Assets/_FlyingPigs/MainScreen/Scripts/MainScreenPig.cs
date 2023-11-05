34baaad7659e50e0a5fd98f3f2077d139f9f52df
Giuseppe Falso <67109504+giuseppe-not-true@users.noreply.github.com>
2023-11-05 13:44:57 +0100

    added animations to initial main screen

--- FlyingPigs/Assets/_FlyingPigs/MainScreen/Scripts/MainScreenPig.cs
+++ FlyingPigs/Assets/_FlyingPigs/MainScreen/Scripts/MainScreenPig.cs
@@ -1,25 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainScreenPig : MonoBehaviour
 {
+    private Vector3 dir;
+    private float speed;
+    private RectTransform rectTransform;
+    private bool isGoingRight = true;
+    private Image pigImage;
+
     void Start()
     {
-        
+        dir = Vector3.right;
+        speed = 250.0f;
+        rectTransform = this.GetComponent<RectTransform>();
+        pigImage = this.GetComponent<Image>();
     }
 
     void Update()
     {
-        var dir = Vector3.right;
-        var speed = 250.0f;
-
-        if(this.transform.position.x >= 1000f){
-            dir = Vector3.left;
-        } else if(this.transform.position.x <= -1000f){
-            dir = Vector3.right;
+        if(isGoingRight){
+            if(rectTransform.localPosition.x >= 1000f){
+                dir = Vector3.left;
+                isGoingRight = false;
+                this.transform.localScale = new Vector3(-this.transform.localScale.x, this.transform.localScale.y);
+            }
+        } else {
+            if(rectTransform.localPosition.x <= -1200f){
+                dir = Vector3.right;
+                isGoingRight = true;
+                this.transform.localScale = new Vector3(-this.transform.localScale.x, this.transform.localScale.y);
+            }
         }
 
-        this.transform.position += dir * speed * Time.deltaTime;
+        rectTransform.localPosition += dir * speed * Time.deltaTime;
+        rectTransform.localPosition += Vector3.up * Mathf.Sin(rectTransform.localPosition.x) * 400 * Time.deltaTime;
     }
 }
